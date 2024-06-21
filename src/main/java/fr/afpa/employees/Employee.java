688550bf4c24d7b6a6b34ea0f655c5638dbb3615
bu-lo <161450788+bu-lo@users.noreply.github.com>
2024-06-21 12:03:54 +0200

    poo td part 1

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -1,5 +1,7 @@
 package fr.afpa.employees;
 
+import java.time.LocalDate;
+
 /*
 Objectif :
 L’objectif de ce TP est de concevoir un programme en console basé sur une approche objet et
@@ -9,31 +11,100 @@ Vous allez écrire une classe représentant les salariés d'une entreprise.
 Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
 */
 
-class Employee
-{
+class Employee {
 	/**
 	 * Matricule de l'employé
 	 */
 	private String registrationNumber;
 
-	// TODO compléter les attributs comme présenté dans le PDF
+	// compléter les attributs comme présenté dans le PDF
+	private String lastName;
+	private String firstName;
+	private double salary;
+	private final int socialParticipation = 30; // ne va pas bouger, non modifiable
+	private LocalDate birthDay;
 
-	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
+	// compléter le constructeur de la classe
+	public Employee(String registrationNumber, String lastName, String firstName, double salary) {
 		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.salary = salary;
+		this.birthDay = LocalDate.parse(birthDay);
+
 	}
 
-	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
+	// implémenter les setters et getters de la classe (permet d'accéder aux
+	// attributs privés)
+	//registrationNumber
 	public String getRegistrationNumber() {
-		return registrationNumber;
+		return this.registrationNumber;
 	}
 
-	public void setRegistrationNumber(String registrationNumber) throws Exception {
+	public void setRegistrationNumber(String registrationNumber) {
 		this.registrationNumber = registrationNumber;
 	}
 
-	// TODO ajouter les getters et les setters
+	//lastName
+	public String getLastName() {
+		return this.lastName;
+	}
 
-	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
-	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	//firstName
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	//salary
+	public double getSalary() {
+		return this.salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	//birthDay
+	public LocalDate getBirthDay(){
+		return birthDay;
+	}
+
+	public void setBirthDay(String birthDay){
+		this.birthDay = LocalDate.parse(birthDay);
+	}
+
+	//Method - Full Name
+
+	public String fullName(){
+		return "Employee Full Name {"
+				+ getLastName() 
+				+ " " + getFirstName()
+				+"}";
+	}
+
+	//Method - Calcul
+
+	// implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui
+	// représente un objet de la classe employé
+	// plus d'information sur la méthode "toString()" ->
+	// https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+
+	@Override
+	public String toString(){
+		return "Employee {"
+				+ "Registration Number: " + getRegistrationNumber()
+				+ ", Last Name: " + getLastName()
+				+ ", First Name: " + getFirstName()
+				+ ", Salary: " + getSalary()
+				+ "}";
+			}
+
 }
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -12,10 +12,60 @@ class EmployeeMain
 	{
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
-		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
+		//instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
+		Employee employee1 = new Employee("11ABC22", "Don", "Michel", 2000.0);
+		Employee employee2 = new Employee("11ABC23", "Joly", "Angelina", 10000.0);
+		Employee employee3 = new Employee("11ABC24", "Pitt", "Brad", 10000.0);
+		Employee employee4 = new Employee("11ABC25", "Bonette", "John", 5000.0);
 
-		// TODO afficher les informations des employés avec System.out.println
+		//afficher les informations des employés avec System.out.println
+
+		System.out.println("Employee n°1:");
+		System.out.println("Registration Number : " + employee1.getRegistrationNumber());
+		System.out.println("Last Name : " + employee1.getLastName());
+		System.out.println("First Name : " + employee1.getFirstName());
+		System.out.println("Salary : " + employee1.getSalary());
+
+		System.out.println("-----------------------------------------------------");
+
+		System.out.println("Employee n°2:");
+		System.out.println("Registration Number : " + employee2.getRegistrationNumber());
+		System.out.println("Last Name : " + employee2.getLastName());
+		System.out.println("First Name : " + employee2.getFirstName());
+		System.out.println("Salary : " + employee2.getSalary());
+
+		System.out.println("-----------------------------------------------------");
+
+		System.out.println("Employee n°3:");
+		System.out.println("Registration Number : " + employee3.getRegistrationNumber());
+		System.out.println("Last Name : " + employee3.getLastName());
+		System.out.println("First Name : " + employee3.getFirstName());
+		System.out.println("Salary : " + employee3.getSalary());
+
+		System.out.println("-----------------------------------------------------");
+
+		System.out.println("Employee n°4:");
+		System.out.println("Registration Number : " + employee4.getRegistrationNumber());
+		System.out.println("Last Name : " + employee4.getLastName());
+		System.out.println("First Name : " + employee4.getFirstName());
+		System.out.println("Salary : " + employee4.getSalary());
+
+		System.out.println("-----------------------------------------------------");
+
+		System.out.println(employee1);
+		System.out.println(employee2);
+		System.out.println(employee3);
+		System.out.println(employee4);
+
+		System.out.println("-----------------------------------------------------");
+
+		System.out.println(employee1.fullName());
+		System.out.println(employee2.fullName());
+		System.out.println(employee3.fullName());
+		System.out.println(employee4.fullName());
+
+		System.out.println("-----------------------------------------------------");
+
 	}
 }
 
