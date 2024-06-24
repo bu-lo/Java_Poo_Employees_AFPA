78ebe566d8ffea6f95213089437a3fa1e8f55339
bu-lo <161450788+bu-lo@users.noreply.github.com>
2024-06-24 16:26:21 +0200

    End of TD: setter/array list

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -1,6 +1,7 @@
 package fr.afpa.employees;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /*
 Objectif :
@@ -11,7 +12,7 @@ Vous allez écrire une classe représentant les salariés d'une entreprise.
 Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
 */
 
-class Employee {
+public class Employee {
 	/**
 	 * Matricule de l'employé
 	 */
@@ -25,45 +26,70 @@ class Employee {
 	private LocalDate birthDay;
 
 	// compléter le constructeur de la classe
-	public Employee(String registrationNumber, String lastName, String firstName, double salary) {
+	public Employee(String registrationNumber, String lastName, String firstName, double salary, String birthDay) {
 		this.registrationNumber = registrationNumber;
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.salary = salary;
 		this.birthDay = LocalDate.parse(birthDay);
-
 	}
 
 	// implémenter les setters et getters de la classe (permet d'accéder aux
 	// attributs privés)
-	//registrationNumber
+	// registrationNumber
 	public String getRegistrationNumber() {
 		return this.registrationNumber;
 	}
 
-	public void setRegistrationNumber(String registrationNumber) {
-		this.registrationNumber = registrationNumber;
+	/**
+	 * Setter sur le matricule
+	 * 
+	 * @param registrationNumber Le nouveau matricule (hypoyhétique, à vérifier)
+	 * @throws Exception lancée si le matricule n'est pas bon
+	 */
+	// *** IMPORTANT ***
+	public void setRegistrationNumber(String registrationNumber) throws Exception {
+		// A ce niveau, tentative de modification du matricule
+		// Nous devons vérifier le matricule !
+		// ------> PROGRAMMING DEFENSIVE = vérification des paramètres d'entrée ->
+		// registrationNumber
+		if (checkRegistrationNumber(registrationNumber) == true) {
+			// VRAI donc matricule correct = Modification matricule
+			this.registrationNumber = registrationNumber;
+		} else {
+			// FAUX donc on fait qqch pour traiter ce cas
+			// EXCEPTION = ERREUR
+			throw new Exception("ERROR: The new registration number isn't valid !");
+		}
 	}
 
-	//lastName
+	// lastName
 	public String getLastName() {
 		return this.lastName;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setLastName(String lastName) throws Exception {
+		if (checkName(lastName) == true) {
+			this.lastName = lastName;
+		} else {
+			throw new Exception("ERROR: the lastName isn't valid !");
+		}
 	}
 
-	//firstName
+	// firstName
 	public String getFirstName() {
 		return this.firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	public void setFirstName(String firstName) throws Exception {
+		if (checkName(firstName) == true) {
 		this.firstName = firstName;
+		}else{
+			throw new Exception("ERROR: the lastName isn't valid !");
+		}
 	}
 
-	//salary
+	// salary
 	public double getSalary() {
 		return this.salary;
 	}
@@ -72,25 +98,161 @@ class Employee {
 		this.salary = salary;
 	}
 
-	//birthDay
-	public LocalDate getBirthDay(){
+	// birthDay
+	public LocalDate getBirthDay() {
 		return birthDay;
 	}
 
-	public void setBirthDay(String birthDay){
-		this.birthDay = LocalDate.parse(birthDay);
+	public void setBirthDay(String birthDay) throws Exception{
+		if(checkBirthDate(birthDay) == true){
+			this.birthDay = LocalDate.parse(birthDay);
+		}else{
+			throw new Exception("ERROR: the Birth Date isn't valid !");
+		}
+		
 	}
 
-	//Method - Full Name
-
-	public String fullName(){
+	// Method - Full Name
+	public String fullName() {
 		return "Employee Full Name {"
-				+ getLastName() 
+				+ getLastName()
 				+ " " + getFirstName()
-				+"}";
+				+ "}";
 	}
 
-	//Method - Calcul
+	public double toNetSalary() {
+		return this.salary - this.salary * (this.socialParticipation / 100.0);
+	}
+
+	public long daysBeforeBirthDay() {
+		LocalDate currentDate = LocalDate.now();
+		LocalDate nextBirthDay = this.birthDay.withYear(currentDate.getYear()); // .withYear permet de set l'année
+																				// birthyear sr l'année actuelle ->
+																				// prochain anniversaire
+
+		// Si l'anniversaire de cette année est déjà passsée, on prend l'année prochaine
+		if (nextBirthDay.isBefore(currentDate) || nextBirthDay.isEqual(currentDate)) {
+			nextBirthDay = nextBirthDay.plusYears(1); // .plusYears rajoute une valeur à l'année, idem .plusMonths ou
+														// .plusDays
+		}
+
+		return ChronoUnit.DAYS.between(currentDate, nextBirthDay);
+	}
+
+	/**
+	 * Vérifie une chaîne de caractères et indique s'il s'agit d'un matricuel
+	 * correctement formaté ou non
+	 * 
+	 * @param inputToCheck La chaîne de caractère à vérifier
+	 * @return VRAI s'il s'agit d'un matricule, FAUX sinon
+	 */
+	private boolean checkRegistrationNumber(String inputToCheck) {
+
+		// Vérification de la taille de la chaîne de caractères
+		if (inputToCheck.length() != 7) {
+			return false;
+		}
+
+		// déclaration du booléen qui va stocker le résultat de la vérification
+		// VRAI -> la chaîne de caractères passée en paramètre est un matricule
+		// correctement formaté
+		// FAUX -> la chaîne de caractères passée en paramètre est un matricule pas bien
+		// formaté
+		boolean isRegistrationNumber = false;
+
+		// Cette première boucle permet de passer en revue TOUS les caractères de la
+		// chaîne
+		for (int index = 0; index < inputToCheck.length(); index++) {
+			// index = longueur de intupToCheck - 1 = 6
+			char ch = inputToCheck.charAt(index);
+			if (index == 0 || index == 1 || index == 5 || index == 6) {
+
+				// vérification du caractère, s'agit-il d'un chiffre ?
+				if (Character.isDigit(ch) == true) {
+					isRegistrationNumber = true;
+				} else { // attention, ce n'est pas un chiffre
+					return false;
+				}
+
+			} else { // cas de la position 2, 3 ou 4 -> vérification de letter
+
+				// Quelle opération dois-je faire ?
+				if (Character.isLetter(ch)) {
+					isRegistrationNumber = true;
+				} else {
+					return false;
+				}
+			}
+		} // fin du FOR, BRAVO !
+
+		return isRegistrationNumber;
+	}
+
+	/**
+	 * Vérifie qu'une chaine de caractères passée en paramètre est un prénom ou un nom
+	 * Pas de chiffres + caractères spé sauf '-'
+	 * 
+	 * @param inputToCheck La chaîne de caractères à vérifier
+	 * @return VRAI s'il s'agit d'un prénom/nom correctement formaté, FAUX sinon
+	 */
+
+	private boolean checkName(String inputToCheck) {
+		// déclaration du booléen qui va stocker le résultat de la vérification
+		// VRAI -> la chaîne de caractères passée en paramètre est un matricule
+		// correctement formaté
+		// FAUX -> la chaîne de caractères passée en paramètre est un matricule pas bien
+		// formaté
+
+		boolean isName = false;
+		for (int index = 0; index < inputToCheck.length(); index++) {
+			char ch = inputToCheck.charAt(index);
+			if (Character.isLetter(ch) == true || ch == '-') { // rappel || -> ou
+				isName = true;
+			} else {
+				return false;
+			}
+		}
+		return isName;
+	}
+
+	/**
+	 * Vérifie qu'une chaine de caractères passée en paramètre est une date de naissance
+	 *
+	 * @param inputToCheck La chaîne de caractères à vérifier
+	 * @return VRAI s'il s'agit d'une date de naissance correctement formaté, FAUX sinon
+	 */
+
+	private boolean checkBirthDate(String inputToCheck) {
+
+		if (inputToCheck.length() != 10) {
+			return false;
+		}
+
+		boolean isBirthDate = false;
+		for (int index = 0; index < inputToCheck.length(); index++) {
+			// index = longueur de intupToCheck - 1 = 9
+			char ch = inputToCheck.charAt(index);
+			if (index == 0 || index == 1 || index == 2 || index == 3 || index == 5|| index == 6|| index == 8|| index == 9) {
+
+				// vérification du caractère, s'agit-il d'un chiffre ?
+				if (Character.isDigit(ch) == true) {
+					isBirthDate = true;
+				} else { // attention, ce n'est pas un chiffre
+					return false;
+				}
+
+			} else { // cas de la position 4 et 7 -> vérification -
+
+				if (ch == '-') {
+					isBirthDate = true;
+				} else {
+					return false;
+				}
+			}
+		}
+		return isBirthDate;
+	}
+
 
 	// implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui
 	// représente un objet de la classe employé
@@ -98,13 +260,16 @@ class Employee {
 	// https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
 
 	@Override
-	public String toString(){
+	public String toString() {
 		return "Employee {"
 				+ "Registration Number: " + getRegistrationNumber()
 				+ ", Last Name: " + getLastName()
 				+ ", First Name: " + getFirstName()
+				+ ", Social Participation: " + socialParticipation
 				+ ", Salary: " + getSalary()
+				+ ", Net Salary: " + toNetSalary()
+				+ ", Birth Day: " + getBirthDay()
+				+ ", Next Birth Day in: " + daysBeforeBirthDay() + " days. "
 				+ "}";
-			}
-
+	}
 }
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -1,30 +1,69 @@
 package fr.afpa.employees;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
 /**
  * Classe principale du projet, contient la fonction "main"
  */
-class EmployeeMain
-{
-	public static void main(String[] args) 
-	{
+class EmployeeMain {
+	public static void main(String[] args) {
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
-		//instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22", "Don", "Michel", 2000.0);
-		Employee employee2 = new Employee("11ABC23", "Joly", "Angelina", 10000.0);
-		Employee employee3 = new Employee("11ABC24", "Pitt", "Brad", 10000.0);
-		Employee employee4 = new Employee("11ABC25", "Bonette", "John", 5000.0);
+		// instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
+		Employee employee1 = new Employee("11ABC22", "Don", "Michel", 2000.0, "1995-05-22");
+		Employee employee2 = new Employee("11ABC23", "Joly", "Angelina", 10000.0, "1975-06-04");
+		Employee employee3 = new Employee("11ABC24", "Pitt", "Brad", 10000.0, "1963-12-18");
+		Employee employee4 = new Employee("11ABC25", "Bonette", "John", 5000.0, "1995-07-19");
 
-		//afficher les informations des employés avec System.out.println
+		// ex test, modifier registrationNumber avec un setter
+		// pour gérer une exception -> utiliser TRY... CATCH
+		try {
+			employee1.setRegistrationNumber("11ABC21");
+		
+		} catch (Exception exceptionToProcess) {
+			// CODE POUR GERER L'EXCEPTION
+			// dans notre cas juste un message d'erreur
+			exceptionToProcess.getMessage(); // appelle le message de l'exception ***
+		}
+
+		// modifier NOM avec un setter
+		try {
+			employee1.setLastName("Boivin");
+
+		} catch (Exception exceptionToProcess) {
+			// CODE POUR GERER L'EXCEPTION - ici idem message d'erreur
+			exceptionToProcess.getMessage(); // appelle le message de l'exception ***
+		}
+
+		// modifier PRENOM avec un setter
+		try {
+			employee1.setFirstName("Chloé");
+
+		} catch (Exception exceptionToProcess) {
+			// CODE POUR GERER L'EXCEPTION - ici idem message d'erreur
+			exceptionToProcess.getMessage(); // appelle le message de l'exception ***
+		}
+
+		// modifier DATE
+		try {
+			employee1.setBirthDay("1995-07-19");
+
+		} catch (Exception exceptionToProcess) {
+			// CODE POUR GERER L'EXCEPTION - ici idem message d'erreur
+			exceptionToProcess.getMessage(); // appelle le message de l'exception ***
+		}
+
+
+		// afficher les informations des employés avec System.out.println
 
 		System.out.println("Employee n°1:");
 		System.out.println("Registration Number : " + employee1.getRegistrationNumber());
 		System.out.println("Last Name : " + employee1.getLastName());
 		System.out.println("First Name : " + employee1.getFirstName());
 		System.out.println("Salary : " + employee1.getSalary());
+		System.out.println("Net Salary : " + employee1.toNetSalary());
+		System.out.println("Birth Day : " + employee1.getBirthDay());
+		System.out.println("Days before next Birth Day : " + employee1.daysBeforeBirthDay());
 
 		System.out.println("-----------------------------------------------------");
 
@@ -33,6 +72,9 @@ class EmployeeMain
 		System.out.println("Last Name : " + employee2.getLastName());
 		System.out.println("First Name : " + employee2.getFirstName());
 		System.out.println("Salary : " + employee2.getSalary());
+		System.out.println("Net Salary : " + employee2.toNetSalary());
+		System.out.println("Birth Day : " + employee2.getBirthDay());
+		System.out.println("Days before next Birth Day : " + employee2.daysBeforeBirthDay());
 
 		System.out.println("-----------------------------------------------------");
 
@@ -41,6 +83,9 @@ class EmployeeMain
 		System.out.println("Last Name : " + employee3.getLastName());
 		System.out.println("First Name : " + employee3.getFirstName());
 		System.out.println("Salary : " + employee3.getSalary());
+		System.out.println("Net Salary : " + employee3.toNetSalary());
+		System.out.println("Birth Day : " + employee3.getBirthDay());
+		System.out.println("Days before next Birth Day : " + employee3.daysBeforeBirthDay());
 
 		System.out.println("-----------------------------------------------------");
 
@@ -49,13 +94,16 @@ class EmployeeMain
 		System.out.println("Last Name : " + employee4.getLastName());
 		System.out.println("First Name : " + employee4.getFirstName());
 		System.out.println("Salary : " + employee4.getSalary());
+		System.out.println("Net Salary : " + employee4.toNetSalary());
+		System.out.println("Birth Day : " + employee4.getBirthDay());
+		System.out.println("Days before next Birth Day : " + employee4.daysBeforeBirthDay());
 
 		System.out.println("-----------------------------------------------------");
 
-		System.out.println(employee1);
-		System.out.println(employee2);
-		System.out.println(employee3);
-		System.out.println(employee4);
+		System.out.println(employee1.toString());
+		System.out.println(employee2.toString());
+		System.out.println(employee3.toString());
+		System.out.println(employee4.toString());
 
 		System.out.println("-----------------------------------------------------");
 
@@ -66,6 +114,14 @@ class EmployeeMain
 
 		System.out.println("-----------------------------------------------------");
 
+		ArrayList<Employee> listEmployees = new ArrayList<>();
+		listEmployees.add(employee1);
+		listEmployees.add(employee2);
+		listEmployees.add(employee3);
+		listEmployees.add(employee4);
+
+		for (Employee employee : listEmployees) {
+			System.out.println(employee);
+		}
 	}
 }
-
